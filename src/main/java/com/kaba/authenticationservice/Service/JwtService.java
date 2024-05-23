268c3bee6fb7205799033eb2c05c7c2d8f75de94
Abdellah <abdallahkaba98@gmail.com>
2024-05-23 18:54:33 +0000

    extact-claimsFromJwt-SetSigningKeyImpleMethode-extarct-inSingleClaimsFromJwt

--- src/main/java/com/kaba/authenticationservice/Service/JwtService.java
+++ src/main/java/com/kaba/authenticationservice/Service/JwtService.java
@@ -1,10 +1,38 @@
 package com.kaba.authenticationservice.Service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
+
+    private static final String SECRET_KEY = "9K2KyTRY9jOunLi2lFevSauDrB5qk7QguLEtDLxzQH9JH9GKBYNfo6BCRi0rJHgC" ;
     public String extractUsername(String token) {
         return null ;
+    }
+
+    public <T> T extractClaims(String token, Function<Claims,T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims) ;
+    }
+
+    private Claims extractAllClaims (String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInkey())
+                .build()
+                .parseClaimsJwt(token)
+                .getBody();
+    }
+
+    private Key getSignInkey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
