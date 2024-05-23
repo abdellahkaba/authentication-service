5f4b680577c300064e252f2d57984ed604e5891c
Abdellah <abdallahkaba98@gmail.com>
2024-05-23 19:22:07 +0000

    generate-the-jwt-token

--- src/main/java/com/kaba/authenticationservice/Service/JwtService.java
+++ src/main/java/com/kaba/authenticationservice/Service/JwtService.java
@@ -2,11 +2,16 @@ package com.kaba.authenticationservice.Service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -20,6 +25,19 @@ public class JwtService {
     public <T> T extractClaims(String token, Function<Claims,T> claimsResolver){
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims) ;
+    }
+    public String generateToken(
+            Map<String, Object> extractClaims,
+            UserDetails userDetails
+    ){
+        return Jwts
+                .builder()
+                .setClaims(extractClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSignInkey(), SignatureAlgorithm.HS256)
+                .compact();
     }
 
     private Claims extractAllClaims (String token){
