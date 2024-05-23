befd63ead2b1184a7d23067b363adad744b17f25
Abdellah <abdallahkaba98@gmail.com>
2024-05-23 16:50:38 +0000

    create-userRepository

---
+++ src/main/java/com/kaba/authenticationservice/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.kaba.authenticationservice.repository;
+
+import com.kaba.authenticationservice.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email) ;
+}
