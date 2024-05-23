2ca0c11de5b792108ee2fc0583a134993862a3b5
Abdellah <abdallahkaba98@gmail.com>
2024-05-23 22:36:22 +0000

    create-auth-controller-response-request-service

---
+++ src/main/java/com/kaba/authenticationservice/auth/AuthenticationService.java
@@ -0,0 +1,18 @@
+package com.kaba.authenticationservice.auth;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        return null ;
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        return null ;
+    }
+}
