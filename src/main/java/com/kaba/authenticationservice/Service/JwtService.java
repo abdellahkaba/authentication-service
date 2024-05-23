432a87dacace458a29439cc1b126bade2b7ba71a
Abdellah <abdallahkaba98@gmail.com>
2024-05-23 19:28:05 +0000

    check-if-token-isvalid

--- src/main/java/com/kaba/authenticationservice/Service/JwtService.java
+++ src/main/java/com/kaba/authenticationservice/Service/JwtService.java
@@ -26,6 +26,9 @@ public class JwtService {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims) ;
     }
+    public String generateToken(UserDetails userDetails){
+        return generateToken(new HashMap<>(),userDetails);
+    }
     public String generateToken(
             Map<String, Object> extractClaims,
             UserDetails userDetails
@@ -38,6 +41,19 @@ public class JwtService {
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
                 .signWith(getSignInkey(), SignatureAlgorithm.HS256)
                 .compact();
+    }
+
+    public boolean isTokenValid(String token,UserDetails userDetails){
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date()) ;
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaims(token,Claims::getExpiration);
     }
 
     private Claims extractAllClaims (String token){
