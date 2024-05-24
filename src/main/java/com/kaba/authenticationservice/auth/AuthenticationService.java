e28db6d9ab09b20350c5e64949618aa18500394b
Abdellah <abdallahkaba98@gmail.com>
2024-05-24 10:50:42 +0000

    implemente-authenticate-methode

--- src/main/java/com/kaba/authenticationservice/auth/AuthenticationService.java
+++ src/main/java/com/kaba/authenticationservice/auth/AuthenticationService.java
@@ -7,6 +7,8 @@ import com.kaba.authenticationservice.entities.User;
 
 import com.kaba.authenticationservice.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,7 @@ public class AuthenticationService {
     private final UserRepository repository ;
     private final PasswordEncoder passwordEncoder ;
     private final JwtService jwtService ;
+    private final AuthenticationManager manager ;
     public AuthenticationResponse register(RegisterRequest request) {
         var user = User.builder()
                 .firstname(request.getFirstname())
@@ -30,8 +33,19 @@ public class AuthenticationService {
                 .token(jwtToken)
                 .build();
     }
-
-    public AuthenticationResponse authenticate(AuthenticationRequest request) {
-        return null ;
+    public AuthenticationResponse authenticate(AuthenticationRequest request){
+        manager.authenticate(
+            new UsernamePasswordAuthenticationToken(
+                    request.getEmail(),
+                    request.getPassword()
+            )
+        );
+        var user = repository.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
+
 }
